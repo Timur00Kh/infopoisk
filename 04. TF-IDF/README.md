d17c22b60f78365d02bdbe32c61ac30015744021
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-03-31 00:59:16 +0300

    04. TF-IDF

--- 04. TF-IDF/README.md
+++ 04. TF-IDF/README.md
@@ -5,6 +5,7 @@
 1. [Install NodeJs on computer](https://nodejs.org/en/)
 1. Open project directory `cd <RepoRoot>/04. TF-IDF`
 1. Install dependencies `npm i`
-1. Execute command in terminal `"`
+1. Delete file `tfidf.txt` if exists 
+1. Execute command in terminal `node index.js`
 
 Скачать [архив с выкачанными страницами](https://github.com/Timur00Kh/infopoisk/releases/tag/4)
--- 04. TF-IDF/index.js
+++ 04. TF-IDF/index.js
@@ -2,8 +2,35 @@ const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
-const tfIdf = require("tf-idf");
+const tfIdf = require("./TFIDF");
 
-const tokens = String(fs.readFileSync('./tokens.txt'));
+const tokens = fs.readFileSync(
+    './tokens.txt',
+    {encoding: 'UTF-8'}
+    ).split('\n');
 
-console.log(tokens);
+let RESULT = 'word tf idf tf-idf\n';
+console.log(RESULT);
+const DIGITS = 10;
+
+
+(async () => {
+    for (let i = 0; i < tokens.length; i++) {
+        const result = await getRank('./parsed_sites', tokens[i])
+        console.log(`${i + 1}/${tokens.length} ${tokens[i]} ${result.tf.toFixed(DIGITS)} ${result.idf.toFixed(DIGITS)} ${result.tfidf.toFixed(DIGITS)}`)
+        RESULT += `${tokens[i]} ${result.tf.toFixed(DIGITS)} ${result.idf.toFixed(DIGITS)} ${result.tfidf.toFixed(DIGITS)}` + '\n'
+    }
+
+    fs.writeFileSync('./tfidf.txt', RESULT);
+})()
+
+function getRank(path, term) {
+    return new Promise((resolve, reject ) => {
+        tfIdf.getRank(path, term, function (err, docRank) {
+            if (err) {
+                reject(err);
+            }
+            resolve(docRank);
+        });
+    })
+}
