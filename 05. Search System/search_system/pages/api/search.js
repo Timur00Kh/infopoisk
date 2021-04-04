3f151a52ef0411fbda68db161ca7bf8e84a796a0
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-04-05 02:34:32 +0300

    05. Search System

---
+++ 05. Search System/search_system/pages/api/search.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {Client} from 'pg';
+
+const client = new Client();
+client.connect();
+
+export default async (req, res) => {
+    const results = await client.query(
+        `SELECT id,
+                url,
+                text,
+                ts_rank(to_tsvector("text"),
+                        plainto_tsquery($1))
+         FROM document
+         WHERE to_tsvector("text") @@ plainto_tsquery($1)
+         ORDER BY ts_rank(to_tsvector("text"), plainto_tsquery($1)) DESC;`, [req.query.s]);
+
+    res.status(200).json(results)
+}
